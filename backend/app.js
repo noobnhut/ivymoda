371cd30dd214f4a473fefb439ae9eb6cfd75da84
van <nguyenthevann6@gmail.com>
2023-03-19 13:15:15 +0700

    abc

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 require('./Config/connect');
-
+//asdfasdfasdfasdfasdfasdfasdf
 // Thiết lập body-parser
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
