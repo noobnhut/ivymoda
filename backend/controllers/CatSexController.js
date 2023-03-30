69724a1d9cefe58b6b3372b6414b9ef8bc5d3d5b
noobnhut <nguyen.nhut.99.2017@gmail.com>
2023-03-30 09:00:44 +0700

    up catsex

--- backend/controllers/CatSexController.js
+++ backend/controllers/CatSexController.js
@@ -4,8 +4,7 @@ const Cat = db.Categories;
 const Sex = db.Sexes;
 
 
-const getCatSex = async (req, res) => 
-{
+const getCatSex = async (req, res) => {
     try {
         const catsexes = await CatSex.findAll({
             attributes: ['id', 'id_cat', 'id_sex'],
@@ -27,6 +26,74 @@ const getCatSex = async (req, res) =>
         console.log(error);
     }
 }
+const addCatSex = async (req, res) => {
+    try {
+     const {id_cat,id_sex}=req.body;
+     const existingCatSex = await CatSex.findOne({where:{id_cat,id_sex}});
+     if(id_cat == '' && id_sex =='')
+     {
+        res.status(202).json({ message: "Thiếu thông tin nhập vào" });
+            return;
+     }
+     else if(existingCatSex)
+     {
+        return res.status(203).json({ message: 'danh mục đối tượng tồn tại trong hệ thống' });
+
+     }
+     else {
+        const sexcat = await CatSex.create({id_cat,id_sex });
+        return res.status(201).json({ message: "Thêm danh mục đối tượng xong" });
+    }
+    } catch (error) {
+        return res.status(401).json({ message: "Thêm thất bại !" })
+    }
+}
+const updateCatSex = async (req, res) => {
+    const catsex = await CatSex.findOne({ where: { id: req.params.id } });
+    if (!catsex) return res.status(404).json({ message: "Không tìm thấy dữ liệu" });
+
+    const { id_cat,id_sex } = req.body;
+
+    try {
+        const existingCatSex = await CatSex.findOne({ where: { id_cat,id_sex } });
+        if (id_cat == '' && id_sex =='') {
+            res.status(201).json({ message: "Dữ liệu không thể rỗng" });
+        }
+        else if (existingCatSex) {
+            return res.status(203).json({ message: 'danh mục đối tượng đã tồn tại trong hệ thống' });
+        }
+        else {
+            await CatSex.update({ id_cat ,id_sex }, { where: { id: req.params.id } });
+            res.status(200).json({ message: "Cập nhật thành công" });
+        }
+
+    } catch (error) {
+        return res.status(401).json({ message: "Không thể câp nhập danh mục đối tượng" });
+    }
+};
+
+const deleteCatSex = async (req, res) => {
+    const catsex = await CatSex.findOne({
+        where: {
+            id: req.params.id
+        }
+    });
+    if (!catsex) return res.status(404).json({ message: "Không tìm thấy dữ liệu nào" });
+
+    try {
+        await CatSex.destroy({
+            where: {
+                id: req.params.id
+            }
+        });
+        res.status(200).json({ message: "Xóa danh mục đối tượng thành công" });
+    } catch (error) {
+        console.log(error.message);
+    }
+};
 module.exports = {
-    getCatSex
+    getCatSex,
+    addCatSex,
+    deleteCatSex,
+    updateCatSex
 };
--- backend/routes/CatSexRouter.js
+++ backend/routes/CatSexRouter.js
@@ -1,13 +1,16 @@
 const express =require("express") ;
 const {
-    getCatSex
+    getCatSex,
+    addCatSex,
+    deleteCatSex,
+    updateCatSex
 } =require("../controllers/CatSexController.js") ;
 const routerCatSex = express.Router();
 
 routerCatSex.get('/api/getCatSex',getCatSex);
-// routerCatSex.post('/api/addCatSex',addCatSex);
-// routerCatSex.put('/api/updateCatSex/:id',updateCatSex);
-// routerCatSex.delete('/api/deleteCatSex/:id',deleteCatSex);
+routerCatSex.post('/api/addCatSex',addCatSex);
+routerCatSex.put('/api/updateCatSex/:id',updateCatSex);
+routerCatSex.delete('/api/deleteCatSex/:id',deleteCatSex);
 module.exports = { 
     routerCatSex,
     
